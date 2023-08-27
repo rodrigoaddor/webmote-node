f72181fe8bdb6e1f151c05d1eb7eb42ebf8c177d
Rodrigo Addor <me@rodrick.dev>
2023-08-27 17:59:56 -0300

    feat: use protobuf for updates

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 
 import fastify from "fastify";
 import ws from "@fastify/websocket";
-
 import ViGEmClient from "vigemclient";
+import protobuf from "protobufjs";
+
+const messages = await protobuf.load("./data/messages.proto");
+const Update = messages.lookupType("webmote.Update");
 
 let client = new ViGEmClient();
 /** @type {Set<import("vigemclient/lib/X360Controller").X360Controller>} */
@@ -16,43 +19,40 @@ if (connErr !== null) {
 }
 
 const server = fastify();
-server.register(ws);
+server.register(ws, {connectionOptions: {readableObjectMode: true}});
 
 server.register(async (server) => {
   server.get("/ws", { websocket: true }, (connection) => {
+    connection.socket.binaryType = "arraybuffer";
+
     let controller = client.createX360Controller();
 
     let err = controller.connect();
 
     if (err) {
-      connection.socket.send(
-        JSON.stringify({
-          type: "error",
-          message: err.message,
-        })
-      );
+      console.log(err)
       process.exit(1);
     }
 
     controllers.add(controller);
 
-    connection.socket.on("message", (message) => {
-      let { type, name, value } = JSON.parse(message);
+    connection.socket.on("message", (data) => {
+      const message = Update.decode(new Uint8Array(data));
+      const { button, axis } = Update.toObject(message, {enums: String});
 
-      console.log({ type, name, value });
+      if (button) {
+        controller.button[button.name].setValue(button.pressed);
+      }
 
-      if (type === "button") {
-        controller.button[name.toUpperCase()].setValue(value);
-      } else if (type === "axis") {
-        const [x, y] = value;
-        if (["left", "right"].includes(name)) {
-          controller.axis[`${name}X`].setValue(x);
-          controller.axis[`${name}Y`].setValue(y);
+      if (axis) {
+        if (["left", "right"].includes(axis.name)) {
+          controller.axis[`${axis.name}X`].setValue(axis.x);
+          controller.axis[`${axis.name}Y`].setValue(axis.y);
         }
       }
     });
 
-    connection.socket.on("close", () => {
+    connection.socket.on("close", (a, b) => {
       controller.disconnect();
       controllers.delete(controller);
     });
@@ -78,7 +78,7 @@ server.get("/", async (request, reply) => {
   };
 });
 
-server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
