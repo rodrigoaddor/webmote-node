a5b41b0bf268ea4c0c7f5a5cc0ed1df7e70e2192
Rodrigo Addor <me@rodrick.dev>
2023-08-29 12:46:12 -0300

    feat: use webrtc for controller updates

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import fastify from "fastify";
 import ws from "@fastify/websocket";
 import ViGEmClient from "vigemclient";
 import protobuf from "protobufjs";
+import wrtc from "wrtc";
+
+const { RTCPeerConnection } = wrtc;
 
 const messages = await protobuf.load("./data/messages.proto");
 const Update = messages.lookupType("webmote.Update");
@@ -19,42 +22,93 @@ if (connErr !== null) {
 }
 
 const server = fastify();
-server.register(ws, {connectionOptions: {readableObjectMode: true}});
+server.register(ws, { connectionOptions: { readableObjectMode: true } });
+
+const handleUpdate = (controller, { button, axis }) => {
+  if (button) {
+    controller.button[button.name].setValue(button.pressed);
+  }
+
+  if (axis) {
+    if (["left", "right"].includes(axis.name)) {
+      controller.axis[`${axis.name}X`].setValue(axis.x);
+      controller.axis[`${axis.name}Y`].setValue(axis.y);
+    }
+  }
+};
+
+const initWebRTC = () => {
+  const servers = process.env.WEBMOTE_ICE_SERVERS?.split(",") ?? [];
+  const peerConnection = new RTCPeerConnection({
+    iceServers: servers.map((server) => ({ urls: server })),
+  });
+
+  peerConnection.onconnectionstatechange = () => {
+    console.log("wRTC", peerConnection.connectionState);
+  };
+
+  return peerConnection;
+};
 
 server.register(async (server) => {
   server.get("/ws", { websocket: true }, (connection) => {
-    connection.socket.binaryType = "arraybuffer";
+    /** @type {RTCPeerConnection} */
+    let peerConnection;
 
-    let controller = client.createX360Controller();
+    connection.socket.on("message", async (data) => {
+      try {
+        const message = JSON.parse(data);
 
-    let err = controller.connect();
+        if (message.offer) {
+          if (peerConnection) peerConnection.close();
+          peerConnection = initWebRTC();
 
-    if (err) {
-      console.log(err)
-      process.exit(1);
-    }
+          peerConnection.ondatachannel = ({ channel }) => {
+            const controller = client.createX360Controller();
+            if (controller.connect()) {
+              console.log(err);
+              process.exit(1);
+            }
 
-    controllers.add(controller);
+            controllers.add(controller);
 
-    connection.socket.on("message", (data) => {
-      const message = Update.decode(new Uint8Array(data));
-      const { button, axis } = Update.toObject(message, {enums: String});
+            channel.onmessage = ({ data }) => {
+              console.log('got data')
 
-      if (button) {
-        controller.button[button.name].setValue(button.pressed);
-      }
+              const message = Update.decode(new Uint8Array(data));
+              const update = Update.toObject(message, {
+                enums: String,
+              });
 
-      if (axis) {
-        if (["left", "right"].includes(axis.name)) {
-          controller.axis[`${axis.name}X`].setValue(axis.x);
-          controller.axis[`${axis.name}Y`].setValue(axis.y);
+              handleUpdate(controller, update);
+            };
+
+            channel.onclose = () => {
+              controller.disconnect();
+              controllers.delete(controller);
+            };
+          };
+
+          peerConnection.onicecandidate = ({ candidate }) => {
+            connection.socket.send(JSON.stringify({ ice: candidate }));
+          };
+
+          await peerConnection.setRemoteDescription(message.offer);
+          const answer = await peerConnection.createAnswer();
+          await peerConnection.setLocalDescription(answer);
+          connection.socket.send(JSON.stringify({ answer }));
         }
+
+        if (message.ice) {
+          await peerConnection?.addIceCandidate(message.ice);
+        }
+      } catch (e) {
+        console.error("failed trying to handle client message", e);
       }
     });
 
-    connection.socket.on("close", (a, b) => {
-      controller.disconnect();
-      controllers.delete(controller);
+    connection.socket.on("close", () => {
+      peerConnection?.close();
     });
   });
 });
